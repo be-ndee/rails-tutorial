a82e237124cb80e9413a4416cd99e0813a24bc7a
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-08 13:09:33 +0200

    Create index view to list notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,12 @@
 class NotesController < ApplicationController
+    def index
+        @notes = Note.all()
+    end
+
     def show
         @note = Note.find {params[:id]}
     end
-    
+
     def new
     end
 
