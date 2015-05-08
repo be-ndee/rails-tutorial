257e93107d4fce970381c7651bda823d9c6e50b2
Andreas Bissinger <mail@bissinger-andreas.de>
2015-05-08 13:55:46 +0200

    Implement destroying notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -33,6 +33,12 @@ class NotesController < ApplicationController
         end
     end
 
+    def destroy
+        @note = Note.find(params[:id])
+        @note.destroy
+        redirect_to notes_path
+    end
+
     private
         def note_params
             params.require(:note).permit(:text, :date)
